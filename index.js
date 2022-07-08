60b994283483f0f6a9d852652b2fd75f333cc36d
optimho <michael@duplessis.nz>
2022-07-08 20:59:04 +1200

    Fixed the iteration error if no movies are found and console logs a warning

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 
 
 
-const fetchData = async (searchTerm) => {
+const fetchData = async (searchTerm) => {               //fetch a list of movies from a movie API site
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '95cacf70',
-            s: searchTerm
-        }
+            s: searchTerm            // API specifies s for searching the index - www.omdbapi.com
+        }                            // for the specification of the API
     });
-    console.log(response.data);
+    if (response.data.Error){
+        console.log('No title with that name found!! try something else')
+        return [];
+    }
+    return response.data.Search
+    // console.log(response.data);
 };
 
 const input = document.querySelector('input');
 
+const onInput = async (event) => {                        // pass function fetch data into a debounce function
+    const movies = await fetchData(event.target.value)   // fetch data from movie API site - search for a list of movies
+    for (movie of movies){
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}"/>
+        <h1>${movie.Title}</h1>
+        `;
+        document.querySelector('#target').appendChild(div);
+    }
+};
+input.addEventListener('input', debounce(onInput,1000)); // listen to the text input box
 
-
-const onInput = debounce(event => {
-    fetchData(event.target.value)
-});
-
-input.addEventListener('input', onInput);
